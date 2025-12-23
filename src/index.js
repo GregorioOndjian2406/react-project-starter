b34bb19d3f9e387e53bf52645545e2fa2d3e5f7f
GregorioOndjian2406 <gmondjian@gmail.com>
2025-12-23 11:55:11 -0300

    comit inicial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+//importamos las librerias correspondientes
+import React from "react";
+import ReactDOM from "react-dom/client";
+//agarramos el componente que esta en el html con id root
+const el = document.getElementById("root");
+//tomamos control del componente
+const root = ReactDOM.createRoot(el);
+//creamos un compnente  
+function App(){
+    const type = "number";
+    return <input type={type} min={0} max={10} />;
+}
+//mostramos el componente en el browser
+root.render(<App />);
